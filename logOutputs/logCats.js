ab7096629a808d8c43d7300057432a2a00e337a2
grossesbewirken <grossesbewirken@gmail.com>
2022-08-12 12:52:22 +0200

    categories added

--- logOutputs/logCats.js
+++ logOutputs/logCats.js
@@ -1,23 +1,35 @@
 //  ============================================================================
-//  FITNESS:
+//  ACTIVITY:
 //  ---------------------------------------------------------------------------
-//
-//
-//
-//
+//  neighborhood watch: health +++          happy           activitiy +++
+//  home safari:        health ++           happy           activitiy ++
+//  take a nap:         health +            happy           activitiy +
+
 
 
 //  ============================================================================
 //  HAPPYNESS:
 //  ----------------------------------------------------------------------------
-//  streicheln:         happy               gesundheit      fitnss
-//  massage:
-//  pfote küssen:
+//  "kiss the paw":     health              happy +++       activitiy
+//  massage:            health              happy ++        activitiy
+//  stroking:           health              happy +         activitiy
+
+
+
 
 
 //  ============================================================================
-//  FOOD:
+//  HEALTH > FOOD > half effect of happyness and fitness:
 //  ----------------------------------------------------------------------------
-//  Katzenfutter:       gesundheit ++       happy +         fitness 
-//  thunfisch:          gesundheit +++      happy ++        fitness
-//  lasagne             gesundheit +        happy +++       fitness
+//  tuna:               health +++          happy           activitiy
+//  canned food:        health ++           happy           activitiy 
+//  lasagne:            health +            happy           activitiy 
+
+
+
+//  ============================================================================
+//  HEALTH > DRINK > half effect of happyness and fitness:
+//  ----------------------------------------------------------------------------
+//  water:              health +++          happy +         activitiy 
+//  milk:               health ++           happy ++        activitiy
+//  champagne:          health +            happy +++       activitiy
