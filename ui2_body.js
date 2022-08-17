03cfea9f3567ec27422db6fb50e15e348245d8d5
grossesbewirken <grossesbewirken@gmail.com>
2022-08-17 14:31:34 +0200

    prettier

--- ui2_body.js
+++ ui2_body.js
@@ -1,113 +1,117 @@
 const printCatHappy = () => {
-     return`
------X-_                                 X  
------XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
------X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
------X   >                           <   X          X-         -X     |             |
------X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
------X      X        X   X        X      X            X-        -X    |  easy       |
------X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
------X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
-------X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
-------xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
-----X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
----X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
-----X       XXx     V     V       xXX       X-       X                |             |
------Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
----------XXxxx=________________=xxXXX-         vVVv                       
-----------vVVv                  vVVv
+  return`
+     X-_                                 X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+     X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
+     X   >                           <   X          X-         -X     |             |
+     X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
+     X      X        X   X        X      X            X-        -X    |  easy       |
+     X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
+     X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
+      X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
+      xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
+    X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
+   X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
+    X       XXx     V     V       xXX       X-       X                |             |
+     Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
+         XXxxx=________________=xxXXX-                       
+
 `};
 
 
 
-
-const printCatAngry = () => {
+const printCatNormal = () => {
   return`
------X-_                                 X  
------XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
------X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
------X   >                           <   X          X-         -X     |             |
------X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
------X      X        X   X        X      X            X-        -X    |  easy       |
------X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
------X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
-------X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
-------xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
-----X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
----X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
-----X       XXx     V     V       xXX       X-       X                |             |
------Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
----------XXxxx=________________=xxXXX-         vVVv                       
-----------vVVv                  vVVv
+     X-_                                 X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+     X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
+     X   >                           <   X          X-         -X     |             |
+     X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
+     X      X        X   X        X      X            X-        -X    |  easy       |
+     X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
+     X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
+      X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
+      xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
+    X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
+   X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
+    X       XXx     V     V       xXX       X-       X                |             |
+     Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
+          XXxxx=________________=xxXXX-         vVVv                       
+
 `};
 
 
 
 const printCatBored = () => {
      return`
------X-_                                 X  
------XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
------X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
------X   >                           <   X          X-         -X     |             |
------X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
------X      X        X   X        X      X            X-        -X    |  easy       |
------X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
------X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
-------X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
-------xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
-----X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
----X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
-----X       XXx     V     V       xXX       X-       X                |             |
------Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
----------XXxxx=________________=xxXXX-         vVVv                       
-----------vVVv                  vVVv
+     X-_                                 X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+     X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
+     X   >                           <   X          X-         -X     |             |
+     X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
+     X      X        X   X        X      X            X-        -X    |  easy       |
+     X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
+     X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
+      X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
+      xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
+    X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
+   X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
+    X       XXx     V     V       xXX       X-       X                |             |
+      Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
+           XXxxx=________________=xxXXX-         vVVv                       
+            vVVv                  vVVv
 `};
 
 
 
-
-const printCatNormal = () => {
+const printCatAngry = () => {
   return`
------X-_                                 X  
------XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
------X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
------X   >                           <   X          X-         -X     |             |
------X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
------X      X        X   X        X      X            X-        -X    |  easy       |
------X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
------X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
-------X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
-------xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
-----X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
----X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
-----X       XXx     V     V       xXX       X-       X                |             |
------Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
----------XXxxx=________________=xxXXX-         vVVv                       
-----------vVVv                  vVVv
+     X-_                                 X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+     X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
+     X   >                           <   X          X-         -X     |             |
+     X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
+     X      X        X   X        X      X            X-        -X    |  easy       |
+     X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
+     X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
+      X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
+      xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
+    X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
+   X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
+    X       XXx     V     V       xXX       X-       X                |             |
+     Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
+         XXxxx=________________=xxXXX-         vVVv                       
+          vVVv                  vVVv
 `};
 
 
 
-const printCatHappy = () => {
+const printCatHate = () => {
   return`
------X-_                                 X  
------XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
------X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
------X   >                           <   X          X-         -X     |             |
------X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
------X      X        X   X        X      X            X-        -X    |  easy       |
------X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
------X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
-------X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
-------xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
-----X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
----X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
-----X       XXx     V     V       xXX       X-       X                |             |
------Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
----------XXxxx=________________=xxXXX-         vVVv                       
-----------vVVv                  vVVv
+     X-_                                 X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+     X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
+     X   >                           <   X          X-         -X     |             |
+     X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
+     X      X        X   X        X      X            X-        -X    |  easy       |
+     X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
+     X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
+      X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
+      xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
+    X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
+   X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
+    X       XXx     V     V       xXX       X-       X                |             |
+     Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
+          XXxxx=________________=xxXXX-         vVVv                       
+           vVVv                  vVVv
 `};
 
 
 
-console.log(printCatFriendly());
+function printCat ( health ) {
+  if ( health > 95){
+    printCatHappy();
+  } else if ( health <  {
+
+  }
+}
