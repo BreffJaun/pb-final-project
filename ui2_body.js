f22c6b15cd2a10595dc2b4d2a411d40dbebe6d1b
grossesbewirken <grossesbewirken@gmail.com>
2022-08-17 15:47:50 +0200

    prettier

--- ui2_body.js
+++ ui2_body.js
@@ -1,18 +1,21 @@
+import yoshi from './index.js'
+
 const printCatHappy = () => {
   return`
-     X-_                                 X  
-     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
-     X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
-     X   >                           <   X          X-         -X     |             |
-     X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
+
+     X-_                                _X            _=vVVVv=_
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_  _= XX          x-         x       -= G A M E =- 
+     X  >_xX                       Xx_<  X         X            X     ●-====xXx====-●
+     X        =vVVv=       =vVVv=        X          X-          -x    |             |
+     X       X       X   X       X       X           x-         -X    |  Mode:      |
      X      X        X   X        X      X            X-        -X    |  easy       |
-     X     X         |   |         X     XXXxx=_       X-       -X    |  ---------  |
-     X     |     (●) |   | (●)     |     X       Xx    X-       -X    |  Time:      |
-      X    X         X   X         x     X         XX-X-       -X     |  08:16 min  |
-      xX     >=--=<X   ▼   x>=--=<      Xx_          Xx-     -xX      |  ---------  |
-    X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
-   X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
-    X       XXx     V     V       xXX       X-       X                |             |
+     X                 ▼                 XXXxx=_       X-       -X    |  ---------  |
+     X            ===> Y <===            X       Xx    X-       -x    |  Time:      |
+      X          (_____|_____)           X.        XX-X-       -X     |  08:16 min  |
+      xX           V       V             Xx_.        Xx-     -xX      |  ---------  |
+    X  XX                              XX  X--       X-   -xXX        |  Services:  |
+   X    XXx                         xXX     X-.      X_xxXX-          |  121        |
+    X       XXx                   xXX       X-.      X                |             |
      Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
          XXxxx=________________=xxXXX-                       
 
@@ -22,8 +25,9 @@ const printCatHappy = () => {
 
 const printCatNormal = () => {
   return`
-     X-_                                 X  
-     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+
+     X-_                                _X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_  _= XX            _=vVVVv=_        -= G A M E =- 
      X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
      X   >                           <   X          X-         -X     |             |
      X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
@@ -44,8 +48,9 @@ const printCatNormal = () => {
 
 const printCatBored = () => {
      return`
-     X-_                                 X  
-     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+
+     X-_                                _X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_  _= XX            _=vVVVv=_        -= G A M E =- 
      X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
      X   >                           <   X          X-         -X     |             |
      X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
@@ -57,17 +62,18 @@ const printCatBored = () => {
     X  XX         ===> Y <===          XX  X-        X-   -xXX        |  Services:  |
    X    XXx        (___|___)        xXX     X-       X_xxXX-          |  121        |
     X       XXx     V     V       xXX       X-       X                |             |
-      Xx-_      XXXx=-------==xxXXX     _-xXX_____=xXX                 ●-====xXx====-●
+     Xx-_     XXXx=-------==xxXXX      _-xXX_____=xXX                 ●-====xXx====-●
            XXxxx=________________=xxXXX-         vVVv                       
-            vVVv                  vVVv
+
 `};
 
 
 
 const printCatAngry = () => {
   return`
-     X-_                                 X  
-     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
+
+     X-_                                _X  
+     XX  -_ _-=-vvvVVVVVVVVVVvvv=-_  _= XX            _=vVVVv=_        -= G A M E =- 
      X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
      X   >                           <   X          X-         -X     |             |
      X        =vVVv=       =vVVv=        X          Xx-         -X    |  Mode:      |
@@ -88,6 +94,7 @@ const printCatAngry = () => {
 
 const printCatHate = () => {
   return`
+
      X-_                                 X  
      XX  -_ _-=-vvvVVVVVVVVVVvvv=-_     XX            _=vVVVv=_        -= G A M E =- 
      X Xv_xX                       Xx_vX X           x         X      ●-====xXx====-●
@@ -108,10 +115,16 @@ const printCatHate = () => {
 
 
 
-function printCat ( health ) {
-  if ( health > 95){
+function printCat ( yoshi.health ) {
+  if ( yoshi.health > 90){
     printCatHappy();
-  } else if ( health <  {
-
+  } else if ( yoshi.health > 60 ) {
+    printCatNormal();
+  } else if ( yoshi.health > 35) {
+    printCatBored
+  } else if ( yoshi.health > 10) {
+    printCatAngry
+  } else if ( yoshi.health > 0 ) {
+    printCatHate
   }
 }
