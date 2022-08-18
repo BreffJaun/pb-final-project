252279f52e2e65d8633a7a024dd217f1f6e67866
Florian <f.mewes87@gmail.com>
2022-08-18 16:40:41 +0200

    repaired breakpoints

--- ui_determine_warnings.js
+++ ui_determine_warnings.js
@@ -6,12 +6,19 @@ import printMinusLine from './ui_minusLine.js';
 
 // yoshi.food, yoshi.drink, yoshi.mood, yoshi.health
 const needs_terminal_warnings = () => {
-    // HEALTH
+    // HEALTH    
+    if ( yoshi.mood <= 0 || yoshi.food <= 0 || yoshi.drink <= 0 || yoshi.health <= 0 ){
+        return  console.log(), 
+        console.log(), 
+        console.log(), 
+        console.log()
+    }
+
     if (yoshi.health <= 75 && yoshi.health > 65) {
         console.log('     I need a curd mask...with cucumber slices for my eyes!');
     } else if (yoshi.health <= 50 && yoshi.health > 40) {
         console.log('     I need a spa right away');
-    } else if (yoshi.health <= 25) {
+    } else if (yoshi.health <= 25 && yoshi.health > 0) {
         console.log('     I feel dizzy....catch me up')
     } else {
         console.log();
@@ -22,7 +29,7 @@ const needs_terminal_warnings = () => {
         console.log('     Feed me!');
     } else if (yoshi.food <= 55 && yoshi.food > 45) {
         console.log('     I am bored...just a reminder');
-    } else if (yoshi.food <= 35) {
+    } else if (yoshi.food <= 35 && yoshi.food > 0) {
         console.log('     I can see my paws after a long time, so TIME TO FEED ME NOW!');
     } else {
         console.log();
@@ -33,7 +40,7 @@ const needs_terminal_warnings = () => {
         console.log('     Give me something to drink');
     } else if (yoshi.drink <= 45 && yoshi.drink > 35) {
         console.log('     My delicate royal lips are a little too dry');
-    } else if (yoshi.drink <= 20) {
+    } else if (yoshi.drink <= 20 && yoshi.drink > 0) {
         console.log('     I dehydrate....I already see flying mices');
     } else {
         console.log()
@@ -44,11 +51,12 @@ const needs_terminal_warnings = () => {
         console.log('     Entertain me!');
     } else if (yoshi.mood <= 60 && yoshi.mood > 50) {
         console.log('     Hello...i am hungry!');
-    } else if (yoshi.mood <= 30) {
+    } else if (yoshi.mood <= 30 && yoshi.mood > 0) {
         console.log('     Where is the jester?');
     } else {
         console.log();
     }
+
 };
 
 export default needs_terminal_warnings;
