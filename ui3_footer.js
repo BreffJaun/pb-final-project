d9a7d302ae7d936c1ffe924d552bced856d1f178
grossesbewirken <grossesbewirken@gmail.com>
2022-08-18 13:12:36 +0200

    prettier

--- ui3_footer.js
+++ ui3_footer.js
@@ -8,22 +8,8 @@ function printMenu  () {
       { 4 } Cat Food                 { 5 } Cool Fresh Milk          { 6 } Dance for me!
       { 1 } Hot Lasagne              { 2 } Nice Champagne           { 3 } Stroke my Belly!
 
-                                            You can't stop the time!
+                                     YOU CAN'T STOP THE TIME!                                     
 `};
 
 
-// hier kommen die Kommentare
-
-
-const printMinusLine = () => {
-  return `
-- - - - - - - - - - - - - - - - - - - - - - --=xXx=-- - - - - - - - - - - - - - - - - - - - - -`
-};
-
-// hier kommt die Console
-
-
-
 export default printMenu;
-
-export default printMinusLine;
